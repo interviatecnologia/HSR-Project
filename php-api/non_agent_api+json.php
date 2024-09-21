ed77195c8616467d112a6f238dc59be2f8e8f940
interviatecnologia <164801716+interviatecnologia@users.noreply.github.com>
2024-09-21 09:29:49 -0300

    commit

--- php-api/non_agent_api+json.php
+++ php-api/non_agent_api+json.php
@@ -271,6 +271,56 @@ if ($response !== null) {
 
 #######################################################FIM####################################################################
 
+#######################################################JSON COM ARRAYFIM####################################################################
+###ADDLEAD - LISTA
+
+$json = json_decode(file_get_contents('php://input'), true);
+
+$source = $json['source'];
+$user = $json['user'];
+$pass = $json['pass'];
+$function = $json['function'];
+$campaign_id = $json['campaign_id'];
+$list_id = $json['list_id'];
+
+if (isset($json['leads'])) {
+    $leads = $json['leads'];
+    foreach ($leads as $lead) {
+        $first_name = $lead['first_name'];
+        $last_name = $lead['last_name'];
+        $phone_number = $lead['phone_number'];
+        $phone_code = $lead['phone_code'];
+
+        // Verificar se o número de telefone tem o comprimento correto
+        if (strlen($phone_number) < 8 || strlen($phone_number) > 15) {
+            // Retorne um erro ou uma mensagem de erro
+            echo json_encode(array('error' => 'INVALID PHONE NUMBER LENGTH'));
+            exit;
+        }
+
+        // Processar o lead aqui
+        // ...
+    }
+} else {
+    // Código anterior para lidar com um único lead
+    $first_name = $json['first_name'];
+    $last_name = $json['last_name'];
+    $phone_number = $json['phone_number'];
+    $phone_code = $json['phone_code'];
+
+    // Adicionar lead individualmente
+    $add_lead_response = add_lead($first_name, $last_name, $phone_number, $phone_code, $campaign_id, $list_id);
+    if ($add_lead_response) {
+        echo json_encode(array('success' => 'LEAD HAS BEEN ADDED - ' . $phone_number));
+    } else {
+        echo json_encode(array('error' => 'FAILED TO ADD LEAD'));
+    }
+}
+
+
+
+#######################################################FIM####################################################################
+
 
 ### If you have globals turned off uncomment these lines
 if (isset($_GET["user"]))						{$user=$_GET["user"];}
@@ -1458,7 +1508,6 @@ if ( ($api_list_restrict > 0) and ( ($function == 'add_lead') or ($function == '
 		}
 	}
 ##### END user authentication for all functions below #####
-
 
 
 ################################################################################
